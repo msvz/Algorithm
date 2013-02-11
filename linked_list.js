64cedb22bf03882ebe3d2eaeacb96cc2bf2c86c2
msvz <musicsouzl@gmail.com>
2013-02-10 21:36:08 -0800

    Added delete element method to the LinkedList

--- linked_list.js
+++ linked_list.js
@@ -8,13 +8,14 @@ function Node(data, link) {
 
 function LinkedList(node) {  
 	this.head = node || null;
-	if (head === null) this.count = 0;
+	if (this.head === null) this.count = 0;
 	else this.count = 1;
 }
 
 LinkedList.prototype.append = function(node) {	
 	if (this.head === null) {// Traverse the list and add at the end
-		this.head = node; 
+		this.head = node;
+    this.count++;
 		return;
 	}
 	var temp = this.head;
@@ -24,6 +25,31 @@ LinkedList.prototype.append = function(node) {
 	temp.link = node;
 	node.link = null;
 	this.count++;
+}
+
+LinkedList.prototype.deleteElement = function(node) {
+   if (this.head === node) {
+    var new_head = this.head.link;
+    delete this.head['data'];
+    delete this.head['link'];  
+    this.head = new_head;
+    this.count--;
+    return;
+  }
+
+  var curr = this.head;
+
+  while (curr.link != null) {
+    if (curr.link == node) {
+      var temp_node = curr.link;
+      curr.link = temp_node.link 
+      delete temp_node.data;
+      delete temp_node.link;
+      this.count--;
+      break;
+    } 
+  }
+
 }
 
 LinkedList.prototype.display = function() {
@@ -52,10 +78,8 @@ LinkedList.prototype.reverse = function() {
 	if (prev !== null) this.head = prev;
 }
 
+
 LinkedList.prototype.addatbeg = function() {
-};
-	
-LinkedList.prototype.deleteElem = function(data) {	
 };
 
 LinkedList.prototype.addafter = function() {
@@ -66,7 +90,7 @@ var first_node = new Node(1),
 	second_node = new Node(2),
 	third_node = new Node(3);
 	
-var list = new LinkedList(),
+var list = new LinkedList();
 	list.append(first_node);
 	list.append(second_node);
 	list.append(third_node);
@@ -82,3 +106,8 @@ var list_with_one_node = new LinkedList(new Node(1));
 list_with_one_node.display();
 list_with_one_node.reverse();
 list_with_one_node.display();
+
+list.display();
+list.deleteElement(second_node);
+list.display();
+console.log("List count " + list.count);
